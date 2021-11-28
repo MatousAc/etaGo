2bde74f81478fe0795d3499e08f11157a021c27b
MatousAc <matoush@southern.edu>
2021-11-28 17:32:14 -0500

    automatic connection

--- fisherface.py
+++ fisherface.py
@@ -1,30 +1,38 @@
 # this file defines the structure of a fisher agent
 import asyncio, websockets as ws
-import socket, uuid, random, json, enum
+import socket, uuid, random, json
+import enum, time
 
 class fisher:
-  HOST = "localhost" #'127.0.0.1'
+  HOST = "127.0.0.1"
   PORT = 4444
   def __init__(self):
     # self.port = random.randint(1024, 65535)
     self.uuid = uuid.uuid4()
     self.sock =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
+    self.game = {}
+    asyncio.run(self.play())
     print(self.uuid)
     print("created fisher")
 
   async def play(self):
     print(f"connecting to: ws://{self.HOST}:{self.PORT}/websocket/{self.uuid}")
-    async with ws.connect(f"ws://{self.HOST}:{self.PORT}/websocket/{self.uuid}") as sock:
-      print(sock)
-      await sock.send("PING")
-      await sock.recv()
+    self.sock = await ws.connect(f"ws://{self.HOST}:{self.PORT}/websocket/{self.uuid}")
+    while True:
+      await self.sock.send("PING")
+      update = json.loads(await self.sock.recv())
+      if (update != self.game):
+        self.game = update
+        print(self.game)
+        self.handle_states()
+      time.sleep(1)
+
+  def handle_states():
+    
 
   def __del__(self):
     # self.sock.close()
     pass
 
 if __name__ == "__main__":
-  # do my testing stuff
   etaGo = fisher()
-  asyncio.run(etaGo.play())
