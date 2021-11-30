3767df215e049e0090a58a658e2955be39e81b03
MatousAc <matoush@southern.edu>
2021-11-29 19:32:34 -0500

    mu && eta Go init

--- eta.py
+++ eta.py
@@ -4,18 +4,33 @@ class etaGo(fisher):
   def __init__(self):
     self.phands = []
     self.stats = {
-      "num_unknown"   : 52,
+      "num_players"   : 0,
+      "unknown_cards"   : 52,
       "first_pass"    : True
     }
     super().__init__()
 
+  def possibilities_deck(self): # returns an object representing a deck of possibilities
+    deck = {}
+    for rank in ["2", "3", "4", "5", "6", "7", "8", "9", "10", "j", "q", "k", "a"]:
+        for suit in ["diams", "spades", "clubs", "hearts"]:
+            deck[f"{rank} {suit}"] = 1/self.stats["unknown_cards"]
+    return deck
+
   def configure_hands(self):
-    pass
+    self.stats["unknown_cards"] -= len(self.game["hand"])
+    self.stats["num_players"] = len(self.game["other_hands"]) + 1
+    for pid in range(self.stats["num_players"]):
+      if pid != self.game["pid"]:
+        self.hands[pid] = None
+      self.hands[pid] = self.possibilities_deck()
+
 
   def think(self):
     print("overridden think")
     if self.stats["first_pass"]:
       self.configure_hands()
+      self.stats["first_pass"] = False
     pass
 
   def play(self):
